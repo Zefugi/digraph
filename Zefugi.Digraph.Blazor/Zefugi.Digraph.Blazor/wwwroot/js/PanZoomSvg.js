68668ad6f30b7e8b98aab36e0ad1c5735f56a47e
Zefugi <trowi@live.dk>
2020-01-25 10:25:41 +0100

    Implemented a PanZoomSvg component.

---
+++ Zefugi.Digraph.Blazor/Zefugi.Digraph.Blazor/wwwroot/js/PanZoomSvg.js
@@ -0,0 +1,22 @@
+﻿var pzsIsDragging = false;
+
+function pzsMouseDown() {
+    pzsIsDragging = true;
+    console.log("panZoomSvg_mouseDown");
+}
+
+function pzsMouseUp() {
+    pzsIsDragging = false;
+    console.log("panZoomSvg_mouseUp");
+}
+
+function pzsMouseMove() {
+    if (pzsIsDragging) {
+
+        console.log("panZoomSvg_mouseMove");
+    }
+}
+
+function pzsMouseWheel() {
+    console.log("panZoomSvg_mouseWheel");
+}
