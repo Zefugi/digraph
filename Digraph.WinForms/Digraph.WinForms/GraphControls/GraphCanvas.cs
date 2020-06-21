1ceec09be355468515305f99078a524297f1c4e1
Zefugi <trowi@live.dk>
2020-06-21 12:43:15 +0200

    Created a graph canvas and added it to the graph panel.

---
+++ Digraph.WinForms/Digraph.WinForms/GraphControls/GraphCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Digraph.WinForms.GraphControls
+{
+    public class GraphCanvas : UserControl
+    {
+    }
+}
