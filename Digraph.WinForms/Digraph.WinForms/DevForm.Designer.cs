93380393b9ffcafd3b230a38727ae618d27f04c4
Zefugi <trowi@live.dk>
2020-06-21 12:43:00 +0200

    Created a graph panel.

--- Digraph.WinForms/Digraph.WinForms/DevForm.Designer.cs
+++ Digraph.WinForms/Digraph.WinForms/DevForm.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.graphPanel1 = new Digraph.WinForms.GraphPanel();
             this.SuspendLayout();
+            // 
+            // graphPanel1
+            // 
+            this.graphPanel1.AutoScroll = true;
+            this.graphPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphPanel1.Location = new System.Drawing.Point(0, 0);
+            this.graphPanel1.Name = "graphPanel1";
+            this.graphPanel1.Size = new System.Drawing.Size(800, 450);
+            this.graphPanel1.TabIndex = 0;
             // 
             // DevForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.graphPanel1);
             this.Name = "DevForm";
             this.Text = "DevForm";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private GraphPanel graphPanel1;
     }
 }
 
---
+++ Digraph.WinForms/Digraph.WinForms/GraphPanel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using Digraph.WinForms.GraphControls;
+
+namespace Digraph.WinForms
+{
+    public class GraphPanel : Panel
+    {
+        public readonly GraphCanvas Canvas;
+
+        public GraphPanel()
+        {
+            AutoScroll = true;
+            BackColor = Color.Magenta;
+            
+            Canvas = new GraphCanvas() {
+                BackColor = Color.SlateGray,
+                Size = ClientSize,
+            };
+            Controls.Add(Canvas);
+        }
+
+        protected override void OnResize(EventArgs eventargs)
+        {
+            base.OnResize(eventargs);
+            Canvas.Size = ClientSize;
+        }
+    }
+}
