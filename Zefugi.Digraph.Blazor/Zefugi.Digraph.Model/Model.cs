aba1499588e5010b90198dd55c20646f5d4d36e5
Troels Windekilde <trowi@live.dk>
2020-08-21 16:24:43 +0200

    Created a basic page setup. Should probably develop the Model first.

---
+++ Zefugi.Digraph.Blazor/Zefugi.Digraph.Model/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zefugi.Digraph.Model
+{
+    public class Model
+    {
+        public long ID { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Zefugi.Digraph.Blazor/Zefugi.Digraph.Model/Project.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Zefugi.Digraph.Model
+{
+    public class Project
+    {
+        public string Title { get; set; }
+
+        private Dictionary<long, Model> _models { get; set; } = new Dictionary<long, Model>();
+
+        public Dictionary<long, Model>.Enumerator GetEnumerator() => _models.GetEnumerator();
+
+        public Model CreateModel(string title)
+        {
+            if (ModelExists(title))
+                return null;
+
+            var model = new Model()
+            {
+                ID = GetUnusedID(),
+                Title = title,
+            };
+            _models.Add(model.ID, model);
+
+            return model;
+        }
+
+        public void DeleteModel(long id)
+        {
+            if(ModelExists(id))
+            {
+                _models.Remove(id);
+            }
+        }
+
+        public bool ModelExists(long id) => _models.ContainsKey(id);
+
+        public bool ModelExists(string title)
+        {
+            string titleLowerCase = title.ToLower();
+            using (var ptr = _models.GetEnumerator())
+                while (ptr.MoveNext())
+                    if (ptr.Current.Value.Title.ToLower() == titleLowerCase)
+                        return true;
+            return false;
+        }
+
+        private long GetUnusedID()
+        {
+            long id = 0;
+            while (_models.ContainsKey(++id))
+                if (id == long.MaxValue)
+                    return -1;
+            return id;
+        }
+    }
+}
